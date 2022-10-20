194b29275a87273023b7e87643adf2dfba184126
Rachel Rusiecki <racrusiecki@gmail.com>
2022-10-19 20:27:48 -0500

    Adding Methods exercices.

---
+++ intro_to_programming/methods/ex6.rb
@@ -0,0 +1,8 @@
+# What does the following error message tell you?
+
+def calculate_product(number1, number2)
+    number1 * number2
+end
+calculate_product(4)
+
+# You are calling a method called calculate_product that requires two arguments, but you are only providing one.
