75adacb161d8f9614408e23e8ab108bac1affbd2
Rachel Rusiecki <racrusiecki@gmail.com>
2022-10-25 19:30:38 -0500

    Adding Loops & Iterators exercises.

---
+++ intro_to_programming/loops_iterators/ex3.rb
@@ -0,0 +1,12 @@
+# Write a method that counts down to zero using recursion.
+
+def countdown(n)
+    if n <= 0
+        puts n
+    else
+        puts n
+        countdown(n - 1)
+    end
+end
+countdown(18)
+countdown(-3)
