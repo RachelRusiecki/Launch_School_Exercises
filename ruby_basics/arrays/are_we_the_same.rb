06a79cc83fed3dfe9afdee8b3863685a1191da1d
Rachel Rusiecki <racrusiecki@gmail.com>
2022-11-21 20:16:26 -0600

    Adding Arrays exercises.

---
+++ ruby_basics/arrays/are_we_the_same.rb
@@ -0,0 +1,6 @@
+# In the code below, two arrays containing several numbers are assigned to two variables, array1 and array2.
+
+array1 = [1, 5, 9]
+array2 = [1, 9, 5]
+
+p array1 == array2
---
+++ ruby_basics/arrays/favorite_number2.rb
@@ -0,0 +1,6 @@
+# In the code below, a nested array containing three groups of names and numbers is assigned to favorites.
+
+favorites = [["Dave", 7], ["Miranda", 3], ["Jason", 11]]
+flat_favorites = favorites.flatten
+p flat_favorites
+p favorites
