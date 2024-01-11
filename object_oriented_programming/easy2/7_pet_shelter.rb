66a01c895de7ed127d5774ea3a69cafacb221213
Rachel Rusiecki <racrusiecki@gmail.com>
2024-01-10 20:10:49 -0600

    Adding Pet Shelter exercise.

--- object_oriented_programming/easy2/7_pet_shelter.rb
+++ object_oriented_programming/easy2/7_pet_shelter.rb
@@ -1,71 +1,88 @@
 # Consider the following code:
 
 class Pet
-  attr_reader :animal, :name
+  attr_reader :type, :name
 
-  def initialize(animal, name)
-    @animal = animal
+  def initialize(type, name, shelter)
+    @type = type
     @name = name
+    shelter.pets << self
   end
 
   def to_s
-    "a #{animal} named #{name}"
+    "a #{type} named #{name}"
   end
 end
 
 class Owner
   attr_reader :name, :pets
-  
+
   def initialize(name)
     @name = name
     @pets = []
   end
-    
-  def add_pet(pet)
+
+  def add_pet
     @pets << pet
   end
-    
+
   def number_of_pets
     pets.size
   end
-  
+
   def print_pets
     puts pets
   end
 end
-    
+
 class Shelter
+  attr_accessor :owners, :pets
+
   def initialize
-    @owners = {}
+    @owners = []
+    @pets = []
   end
-    
+
   def adopt(owner, pet)
-    owner.add_pet(pet)
-    @owners[owner.name] ||= owner
+    owners << owner if !owners.include?(owner)
+    owner.pets << pet
+    pets.delete(pet)
   end
-    
+
   def print_adoptions
-    @owners.each_pair do |name, owner|
-      binding.pry
-      puts "#{name} has adopted the following pets:"
+    owners.each do |owner|
+      puts "#{owner.name} has adopted the following pets:"
       owner.print_pets
-      puts
+      puts ''
     end
+    puts 'The Animal Shelter has the following unadopted pets:'
+    puts pets
+    puts ''
   end
 end
 
-butterscotch = Pet.new('cat', 'Butterscotch')
-pudding      = Pet.new('cat', 'Pudding')
-darwin       = Pet.new('bearded dragon', 'Darwin')
-kennedy      = Pet.new('dog', 'Kennedy')
-sweetie      = Pet.new('parakeet', 'Sweetie Pie')
-molly        = Pet.new('dog', 'Molly')
-chester      = Pet.new('fish', 'Chester')
+shelter = Shelter.new
+
+butterscotch = Pet.new('cat', 'Butterscotch', shelter)
+pudding      = Pet.new('cat', 'Pudding', shelter)
+darwin       = Pet.new('bearded dragon', 'Darwin', shelter)
+kennedy      = Pet.new('dog', 'Kennedy', shelter)
+sweetie      = Pet.new('parakeet', 'Sweetie Pie', shelter)
+molly        = Pet.new('dog', 'Molly', shelter)
+chester      = Pet.new('fish', 'Chester', shelter)
+biddy        = Pet.new('hamster', 'Biddy', shelter)
+asta         = Pet.new('dog', 'Asta', shelter)
+laddie       = Pet.new('dog', 'Laddie', shelter)
+fluffy       = Pet.new('cat', 'Fluffy', shelter)
+kat          = Pet.new('cat', 'Kat', shelter)
+ben          = Pet.new('cat', 'Ben', shelter)
+chatterbox   = Pet.new('parakeet', 'Chatterbox', shelter)
+bluebell     = Pet.new('parakeet', 'Bluebell', shelter)
 
 phanson = Owner.new('P Hanson')
 bholmes = Owner.new('B Holmes')
+rrusiecki = Owner.new('R Rusiecki')
 
-shelter = Shelter.new
 shelter.adopt(phanson, butterscotch)
 shelter.adopt(phanson, pudding)
 shelter.adopt(phanson, darwin)
@@ -73,9 +90,12 @@ shelter.adopt(bholmes, kennedy)
 shelter.adopt(bholmes, sweetie)
 shelter.adopt(bholmes, molly)
 shelter.adopt(bholmes, chester)
+shelter.adopt(rrusiecki, biddy)
 shelter.print_adoptions
 puts "#{phanson.name} has #{phanson.number_of_pets} adopted pets."
 puts "#{bholmes.name} has #{bholmes.number_of_pets} adopted pets."
+puts "#{rrusiecki.name} has #{rrusiecki.number_of_pets} adopted pets."
+puts "The Animal Shelter has #{shelter.pets.size} unadopted pets."
 
 # Write the classes and methods that will be necessary to make this code run, and print the following output:
 
