75335eed4c219a3ccb2cf37b717165b00b9a0fd1
Rachel Rusiecki <racrusiecki@gmail.com>
2024-03-21 20:12:04 -0500

    Modified Exercise 7.

--- ruby_foundations_more_topics/medium1/7_bubble_sort_with_blocks.rb
+++ ruby_foundations_more_topics/medium1/7_bubble_sort_with_blocks.rb
@@ -48,7 +48,11 @@ def bubble_sort2!(array)
   loop do
     swapped = false
     1.upto(array.size - 1) do |index|
-      next if yield(array[index - 1]) <= yield(array[index])
+      if block_given?
+        next if yield(array[index - 1]) <= yield(array[index])
+      else
+        next if array[index - 1] <= array[index]
+      end
       array[index - 1], array[index] = array[index], array[index - 1]
       swapped = true
     end
