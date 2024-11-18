24d91fac8995eba48fbae0a3521cdb51a7cb7b94
RachelRusiecki <rachrusiecki@gmail.com>
2024-11-17 19:34:29 -0600

    Adding JS Debugging exercises.

---
+++ javascript_fundamentals/debugging/8_expensive_study_preparation.js
@@ -0,0 +1,66 @@
+// We make a few purchases to prepare for our study session, but the amount charged upon checkout seems too high. Run the following code and find out why we are charged incorrectly.
+
+let shoppingCart = [];
+
+const prices = {
+  'notebook': 9.99,
+  'pencil': 1.70,
+  'coffee': 3.00,
+  'smoothie': 2.10,
+};
+
+function price({name}) {
+  return prices[name];
+}
+
+function updateCart(name, amount = 1) {
+  let alreadyInCart = false;
+
+  for (let i = 0; i < shoppingCart.length; i += 1) {
+    let item = shoppingCart[i];
+
+    if (item.name === name) {
+      item.amount = amount;
+      alreadyInCart = true;
+      break;
+    }
+  }
+
+  if (!alreadyInCart) {
+    shoppingCart.push({ name, amount });
+  }
+}
+
+function total() {
+  let total = 0;
+
+  for (let i = 0; i < shoppingCart.length; i += 1) {
+    let item = shoppingCart[i];
+
+    total += (item.amount * price(item));
+  }
+
+  return total.toFixed(2);
+}
+
+function pay() {
+  // omitted
+
+  console.log(`You have been charged $${total()}.`);
+}
+
+function checkout() {
+  pay();
+  console.log(shoppingCart);
+  shoppingCart = [];
+}
+
+updateCart('notebook');
+updateCart('pencil', 2);
+updateCart('coffee', 1);
+
+updateCart('pencil', 0);
+
+checkout();
+
+// 0 is falsy, so pencil amount is set to 1 instead of 0
