bee3c43bb36296f69905379eaed0712ec27d6eda
RachelRusiecki <rachrusiecki@gmail.com>
2024-12-14 16:04:43 -0600

    Adding Linked Lists DSA exercises.

---
+++ data_structures_and_algorithms/linked_lists/4_merge_two_sorted_linked_lists.js
@@ -0,0 +1,102 @@
+/*
+
+Given two sorted singly linked lists, your task is to combine them into a single sorted linked list. The new list should be composed of the nodes from the two original lists and should also be sorted in ascending order (where node values are equal to or increase as you move through the linked list).
+
+*/
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+function createLinkedList(arr) {
+  let head = new ListNode(0);
+  let current = head;
+  arr.forEach(val => {
+    current.next = new ListNode(val);
+    current = current.next;
+  });
+  return head.next;
+}
+
+function printLinkedList(head) {
+  let current = head;
+  let listStr = '';
+  while (current !== null) {
+    listStr += current.val + ' -> ';
+    current = current.next;
+  }
+  listStr += 'null';
+  console.log(listStr);
+}
+
+function mergeSortedLists(list1, list2) {
+  let dummy = new ListNode(0);
+  let curr = dummy;
+
+  while (list1 !== null && list2 !== null) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+    curr = curr.next;
+  }
+
+  curr.next = list1 === null ? list2 : list1;
+
+  return dummy.next;
+}
+
+function mergeSortedListsNoDummy(list1, list2) {
+  let head;
+
+  if (!list1 || !list2) {
+    head = list1 || list2;
+  } else if (list1.val <= list2.val) {
+    head = list1;
+    list1 = list1.next;
+  } else {
+    head = list2;
+    list2 = list2.next;
+  }
+
+  let curr = head;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      curr.next = list1;
+      list1 = list1.next;
+    } else {
+      curr.next = list2;
+      list2 = list2.next;
+    }
+    curr = curr.next;
+  }
+
+  curr.next = list1 ? list2 : list1;
+
+  return head;
+}
+
+let list1 = createLinkedList([1, 3, 5]);
+let list2 = createLinkedList([2, 4, 6]);
+printLinkedList(mergeSortedLists(list1, list2)); // Expected: 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+
+let list3 = createLinkedList([1, 2, 3]);
+let list4 = createLinkedList([]);
+printLinkedList(mergeSortedLists(list3, list4)); // Expected: 1 -> 2 -> 3 -> null
+
+let list5 = createLinkedList([]);
+let list6 = createLinkedList([1]);
+printLinkedList(mergeSortedLists(list5, list6)); // Expected: 1 -> null
+
+let list7 = createLinkedList([1, 5, 9]);
+let list8 = createLinkedList([2, 4, 6, 8, 10]);
+printLinkedList(mergeSortedLists(list7, list8)); // Expected: 1 -> 2 -> 4 -> 5 -> 6 -> 8 -> 9 -> 10 -> null
+
+let list9 = createLinkedList([1, 2, 5]);
+let list10 = createLinkedList([3, 6, 7]);
+printLinkedList(mergeSortedLists(list9, list10)); // Expected: 1 -> 2 -> 3 -> 5 -> 6 -> 7 -> null
